f41a75c1831a07726a658f8673d0ab67443dfb0d
Houssem Mehouachi <fazeputin@yandex.com>
2024-04-20 01:41:13 +0100

    initial commit

---
+++ src/app/components/Navbar/NavBar.tsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import Container from '@/app/components/Container'
+import Logo from '@/app/components/Navbar/Logo'
+import Search from '@/app/components/Navbar/Search'
+
+export default function NavBar() {
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className='py-4 border-b-[1px]  '>
+                <Container>
+                    <div className='
+                    flex 
+                    flex-row 
+                    items-center
+                    justify-between
+                    gap-3
+                    md-gap-0
+                    '> <Logo />
+                        <Search />
+
+                    </div>
+                </Container>
+
+            </div>
+        </div>
+    )
+}
