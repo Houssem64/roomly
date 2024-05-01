50e119515d4559f02ef3a66d2297952bc62c5f65
Houssem Mehouachi <fazeputin@yandex.com>
2024-05-02 00:44:34 +0100

    tweaks

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -7,21 +7,23 @@ import { NextResponse } from "next/server";
 
 export async function POST(
     request: Request
-){
+) {
     const body = await request.json();
     const {
         email,
         name,
         password
-    } = body ;
-const hashedPassword = await bcrypt.hash(password, 12);
+    } = body;
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-const user = await prisma.user.create({
-    data: {
-        email,
-        name,
-        hashedPassword
-    }
-})
-return NextResponse.json(user);
+    const user = await prisma.$transaction([
+        prisma.user.create({
+            data: {
+                email,
+                name,
+                hashedPassword,
+            },
+        }),
+    ]);
+    return NextResponse.json(user);
 }
